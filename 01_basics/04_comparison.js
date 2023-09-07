e6a1e2428fa72eaca22c46b8e1b657672b4f0def
Deepali2526 <144208062+Deepali2526@users.noreply.github.com>
2023-09-07 10:15:08 +0000

    ComparisonOperator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2 == 1)
+// console.log(2!=1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+/*This type of comparsion must be avoided*/
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null>=0)
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+
+// ===
+
+console.log("2" === 2)
