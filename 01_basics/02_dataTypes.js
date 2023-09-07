fe94d3e1c74c743b3f9b833fe621333f69ab8e47
Deepali2526 <144208062+Deepali2526@users.noreply.github.com>
2023-09-07 09:45:01 +0000

    datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3)   //We are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Deepali")
+
+
+let name = "Deepali"
+let age = 18
+let isLoggedIn = false
+let state ;
+
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => unique
+
+
+//object
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
