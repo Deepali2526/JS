1e084d8f6ff2a71f41e1c4debdadb2fcebad9653
Deepali2526 <144208062+Deepali2526@users.noreply.github.com>
2023-09-10 13:24:51 +0000

    While_do_loop_in_js

---
+++ 05_iterations/WhileDoWhileLoop.js
@@ -0,0 +1,28 @@
+
+// while loop
+
+let i = 0
+while (i<=10) {
+    // console.log(`Value of i is ${i}`);
+    i+=2
+    
+}
+
+let myArray = ['flash', 'batsman', 'superman']
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`${myArray[arr]}`);
+    arr+=1
+    
+}
+
+
+//do-while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
